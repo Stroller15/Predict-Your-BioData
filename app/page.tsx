7162db784526400fbc5c113cd56e2e1fa10de347
Your Name <shubhamverma6351@gmail.com>
2023-08-13 11:12:29 +0530

    feat: :rocket: app is build finally going to deploy

--- app/page.tsx
+++ app/page.tsx
@@ -12,22 +12,29 @@ export default function Home() {
     push(`/prediction/${inputValue}`)
   }
 
-  
+
   return (
-    <div>
-      <div>
-        <h1>Enter Your Name</h1>
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <div className="p-4 shadow-md bg-white rounded-md">
+        <h1 className="text-2xl font-semibold mb-4 text-black">
+          Enter Your Name
+        </h1>
+        <form onSubmit={handleSubmit} className="space-y-3">
+          <input
+            type="text"
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
+            className="w-full p-2 border border-gray-300 rounded text-black"
+            placeholder="Type something..."
+          />
+          <button
+            type="submit"
+            className="w-full py-2 px-4 bg-blue-500 hover:bg-blue-600 text-white font-semibold rounded-lg shadow-md focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75"
+          >
+            Submit
+          </button>
+        </form>
       </div>
-      <form onSubmit={handleSubmit}>
-        <input 
-        type="text" 
-        placeholder="Type your name..." 
-        className="text-black"
-        value={inputValue}
-        onChange={(e) => setInputValue(e.target.value)}
-        />
-        <button type="submit">Predict data</button>
-      </form>
     </div>
-  )
+  );
 }
--- app/prediction/[name]/page.tsx
+++ app/prediction/[name]/page.tsx
@@ -1,12 +1,55 @@
 import { FC } from 'react'
 
+const getPredicteAge = async (name: string) => {
+    const res = await fetch(`https://api.agify.io?name=${name}`)
+    return res.json();
+}
+
+const getPredicteGender = async (name: string) => {
+  const res = await fetch(`https://api.genderize.io?name=${name}`);
+  return res.json();
+};
+
+const getPredicteCountry = async (name: string) => {
+  const res = await fetch(`https://api.nationalize.io?name=${name}`);
+  return res.json();
+};
+
 interface params {
     params: {name: string};
 }
 
 
-const page: FC<params> = ({ params }: params) => {
-  return <div>{params.name}</div>;
-};
+const page: FC<params> = async ({ params }: params) => {
+    const ageData = getPredicteAge(params.name);
+    const genderData = getPredicteGender(params.name);
+    const countryData = getPredicteCountry(params.name);
+
+    const [age, gender, country] = await Promise.all([
+        ageData,
+        genderData,
+        countryData,
+    ])
+
+   return (
+    <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl m-3 p-4">
+      <div className="p-8">
+        <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">
+          Personal Info
+        </div>
+        <div className="block mt-1 text-lg leading-tight font-medium text-black">
+          Age: {age?.age}
+        </div>
+        <div className="block mt-1 text-lg leading-tight font-medium text-black">
+          Gender: {gender?.gender}
+        </div>
+        <div className="block mt-1 text-lg leading-tight font-medium text-black">
+          Nationality: {country?.country[0]?.country_id}
+        </div>
+      </div>
+    </div>
+  );
+}
+
 
 export default page
